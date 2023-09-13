12fdd27f952e657ab45c1a40998bf4694cdc2efd
el glioui <elgliouiyoucode@gmail.com>
2023-09-13 23:05:47 +0200

    changement ->

--- exerciceC/main.c
+++ exerciceC/main.c
@@ -7,7 +7,7 @@ int mois;
 int annee;
 }date;
 
-typedef struct etudiant{
+typedef struct personne{
 char nom[100];
 char prenom[100];
 int cne;
@@ -19,27 +19,28 @@ void lire(personne *etudiant[],int N){
  for(i=1;i<N;i++){
    etudiant[i]=(personne *)malloc(sizeof(personne));
     printf("\n enrer le nom d'etudian nemuro %d",&i);
-       scanf("%s",etudiant[i].nom);
+       scanf("%s",&etudiant[i]->nom);
     printf("\n enrer le nom d'etudian nemuro %d",&i);
-       scanf("%s",etudiant[i].prenom);
+       scanf("%s",&etudiant[i]->prenom);
     printf("\n enrer le CNE d'etudian nemuro %d",&i);
-       scanf("%d",etudiant[i].cne);
+       scanf("%d",&etudiant[i]->cne);
     printf("\n Date de naissance");
        printf("\n jour");
-       scanf("%d",&etudiant[i].jour);
+       scanf("%d",&etudiant[i]->date_de_nai.jour);
         printf("\n mois");
-       scanf("%d",&etudiant[i].mois);
+       scanf("%d",&etudiant[i]->date_de_nai.mois);
         printf("\n annee");
-       scanf("%d",&etudiant[i].annee);
+       scanf("%d",&etudiant[i]->date_de_nai.annee);
       }
 }
-int main()
-{  int N;
+ main()
+{
+     int N;
     printf("\n quele le nombre des etudiants");
     scanf("%d", &N);
     personne *etudiant[N];
     printf("enter la liste des etudiants ");
     lire(etudiant,N);
 
-    return 0;
+
 }
